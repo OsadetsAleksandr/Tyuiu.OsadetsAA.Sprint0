da6e068cf8bb08dab2abecb30f6c120895aeb32b
Mechta <Mechta@ASUS>
2024-09-12 21:07:22 +0500

    Add project files.

---
+++ Tyuiu.OsadetsAA.Sprint0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.OsadetsAA.Sprint0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
