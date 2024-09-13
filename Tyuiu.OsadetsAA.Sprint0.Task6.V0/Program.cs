21c2e007fb3d7a95b37f66939ceb2d647ef5267d
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-09-13 18:04:21 +0500

    Task6

---
+++ Tyuiu.OsadetsAA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.OsadetsAA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.OsadetsAA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Сумма элементов массива = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Сумма элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
