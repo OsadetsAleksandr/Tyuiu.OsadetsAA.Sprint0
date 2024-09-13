29a1e7bf47e493c7f437c2c26e39165b3432e450
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-09-13 16:39:55 +0500

    Task4

---
+++ Tyuiu.OsadetsAA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+using Tyuiu.OsadetsAA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.OsadetsAA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
